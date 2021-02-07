7f532256f1d837c410fac3b28d02960eb2e17c71
Piotr Trybisz <ptrybisz@gmail.com>
2021-02-07 21:26:55 +0100

    Added more beeps

--- VLOCrash.cpp
+++ VLOCrash.cpp
@@ -30,12 +30,14 @@ LRESULT CALLBACK Melter(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
 	case WM_TIMER:
 	{
 		HDC Window = GetDC(hWnd);
-		int X = (rand() % ScreenWidth) - (150 / 2),
-			Y = (rand() % 15),
-			Width = (rand() % 150),
-			Height = (rand() %ScreenHeight);
+		int X = (rand() % ScreenWidth) - (300 / 2),
+			Y = (rand() % 300) - (rand() % 300),
+			Width = (rand() % 50),
+
+			Height = (rand() % ScreenHeight);
 		BitBlt(Window, X, Y, Width, Height, Window, X, 0, SRCCOPY);
 		ReleaseDC(hWnd, Window);
+		Beep(rand() % 1000, 50);
 		break;
 	}
 	case WM_DESTROY:
@@ -71,13 +73,15 @@ LRESULT CALLBACK HackedBy(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
 		RECT rect;
 		HBRUSH hBrush;
 		HFONT hFont;
+
 		hdc = BeginPaint(hWnd, &ps);
-		hFont = CreateFont(72, 0, 0, 450, FW_DONTCARE, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_OUTLINE_PRECIS,
+		SetBkMode(hdc, 1);
+		hFont = CreateFont(256, 0, 0, 450, FW_DONTCARE, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_OUTLINE_PRECIS,
 			CLIP_DEFAULT_PRECIS, CLEARTYPE_QUALITY, VARIABLE_PITCH, TEXT("Arial Black"));
 		SelectObject(hdc, hFont);
 		SetRect(&rect, 0, 0, ScreenWidth, ScreenHeight);
-		SetTextColor(hdc, RGB(255, 0, 0));
-		SetBkMode(hdc, 0);
+		SetTextColor(hdc, RGB(0, 255, 0));
+
 		DrawText(hdc, TEXT("Hacked by II Bg"), -1, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
 		DeleteObject(hFont);
 		EndPaint(hWnd, &ps);
@@ -103,6 +107,8 @@ int APIENTRY WinMain(HINSTANCE Inst, HINSTANCE Prev, LPSTR Cmd, int showcmd)
 	ScreenWidth = GetSystemMetrics(SM_CXSCREEN);
 	ScreenHeight = GetSystemMetrics(SM_CYSCREEN);
 
+	Beep(2500, 750);
+
 	WNDCLASS wndClass = { 0, Melter, 0,0, Inst, 0, LoadCursorW(0, IDC_WAIT), 0, 0, L"ScreenMelter" };
 	if (RegisterClass(&wndClass))
 	{
@@ -118,7 +124,7 @@ int APIENTRY WinMain(HINSTANCE Inst, HINSTANCE Prev, LPSTR Cmd, int showcmd)
 			MSG Msg = { 0 };
 			ULONGLONG startTime = GetTickCount64();
 			// Run the melter loop
-			while (Msg.message != WM_QUIT&&GetTickCount64()<startTime+5000)
+			while (Msg.message != WM_QUIT && GetTickCount64() < startTime + 5000)
 			{
 				if (PeekMessage(&Msg, 0, 0, 0, PM_REMOVE))
 				{
